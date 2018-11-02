3aadbd9674b52a5cfeb95cb3cb2f800c0fc19aa0
roi.herzig <roi.herzig@getnexar.com>
2018-11-02 22:05:24 +0200

    Inference BDD batches

--- samples/bdd100k/InferenceBDDBatches.py
+++ samples/bdd100k/InferenceBDDBatches.py
@@ -30,7 +30,7 @@ DEFAULT_DATASET_YEAR = "2017"
 # Dataset path for the data
 BDD_DATASET_DIR = "/data/BDD/bdd100k/"
 ACCIDENTS_DATASET_DIR = "/data/Accidents1K"
-BATCH_SIZE = 32
+BATCH_SIZE = 20
 
 
 def clean_name(name):
